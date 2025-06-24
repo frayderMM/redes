12b930f1e8f8393e10ec8269108b1ee63a9996d5
frayderMM <mezamorvelifrayder2005@gmail.com>
2025-06-24 14:36:45 -0500

    Update quasar.config.js

--- quasar.config.js
+++ quasar.config.js
@@ -42,6 +42,7 @@ export default defineConfig((ctx) => {
         browser: [ 'es2022', 'firefox115', 'chrome115', 'safari14' ],
         node: 'node20'
       },
+      publicPath: '/',
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
